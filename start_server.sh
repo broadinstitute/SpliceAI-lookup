98d7718da9b1173c991d318c7454d76325bf4cf3
bw2 <weisburd@broadinstitute.org>
2020-07-28 15:38:40 -0400

    updated start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,6 @@
 set -x
 
-gunicorn -t 600 -w 2 -b 0.0.0.0:80 server:app
+gunicorn -t 600 -w 2 -b 0.0.0.0:80  -b 0.0.0.0:443 \
+  --keyfile=../spliceailookup-api.broadinstitute.org.key \
+  --certfile=../spliceailookup-api_broadinstitute_org.cert \
+  server:app
