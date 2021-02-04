34168893368844779a3ed8025ffff93ad5b43ba6
bw2 <weisburd@broadinistitute.org>
2021-02-04 02:43:53 +0000

    increase timeout

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 set -x
 
-gunicorn -w 6 -t 600 -b 0.0.0.0:80  -b 0.0.0.0:443 \
+gunicorn -w 6 -t 1000 -b 0.0.0.0:80  -b 0.0.0.0:443 \
   --keyfile=../spliceailookup-api.broadinstitute.org.key \
   --certfile=../spliceailookup-api_broadinstitute_org.cert \
   server:app
