95ac2fa22735f84e4d6bbfd9c00d3271caa3f3bb
bw2 <weisburd@broadinistitute.org>
2021-04-09 17:04:43 -0400

    updated to gencode v37

--- annotations/update_annotation_txt_files.sh
+++ annotations/update_annotation_txt_files.sh
@@ -1,4 +1,4 @@
 set -x
-for p in gencode*v36*.gtf.gz; do
+for p in gencode*v37*.gtf.gz; do
     time python3 convert_gencode_gtf_to_spliceai_annotation_input_file.py $p  | tee process_$(echo $p | sed s/.gtf.gz//).log
 done
