722413d0d316b654dcee37521cd24fc874ffc54b
bw2 <weisburd@broadinistitute.org>
2021-06-15 04:45:36 +0000

    switched to 5 workers to avoid OOM errors during liftover

--- start_server.sh
+++ start_server.sh
@@ -2,7 +2,7 @@ set -x
 
 redis-cli flushall  #  clear all keys from redis
 
-gunicorn -w 6 -t 1800 -b 0.0.0.0:80  -b 0.0.0.0:443 \
+gunicorn -w 5 -t 1800 -b 0.0.0.0:80  -b 0.0.0.0:443 \
   --keyfile=../spliceailookup-api.broadinstitute.org.key \
   --certfile=../spliceailookup-api_broadinstitute_org.cert \
   server:app
