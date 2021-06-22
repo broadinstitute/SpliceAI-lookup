8fa7d02313afa669bc934d28ef5869db7309ff36
bw2 <weisburd@broadinistitute.org>
2021-06-22 13:50:04 +0000

    simplified ..

--- temporarily_disable_liftover_rate_limit.py
+++ temporarily_disable_liftover_rate_limit.py
@@ -4,9 +4,7 @@ import time
 r = redis.Redis(host='localhost')
 
 while True:
-    for key in r.keys("request *"):
-        key = key.decode("UTF-8")
-        if "liftover" in key:
-            print("Deleting key: ", key)
-            r.delete(key)
+    for key in r.keys("request *liftover*"):
+        print("Deleting key: ", key.decode("UTF-8"))
+        r.delete(key)
     time.sleep(1)
