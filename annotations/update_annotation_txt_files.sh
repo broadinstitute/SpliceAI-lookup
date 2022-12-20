11403dc9e06af3accf37ea5b741fbafb7a2486cb
bw2 <ben.weisburd@gmail.com>
2022-12-20 17:01:10 -0500

    update gencode version

--- annotations/update_annotation_txt_files.sh
+++ annotations/update_annotation_txt_files.sh
@@ -1,4 +1,5 @@
+export PYTHONPATH=~/code/methods/:$PYTHONPATH
 set -x
-for p in gencode*v37*.gtf.gz; do
+for p in gencode*v42*.gtf.gz; do
     time python3 convert_gencode_gtf_to_spliceai_annotation_input_file.py $p  | tee process_$(echo $p | sed s/.gtf.gz//).log
 done
