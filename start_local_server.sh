7c88f762362cb927a300910767521be9fd5d709f
bw2 <ben.weisburd@gmail.com>
2023-09-04 21:08:44 -0400

    added comments

--- start_local_server.sh
+++ start_local_server.sh
@@ -1,7 +1,9 @@
 set -x
 
-NUM_THREADS=1
-HOST=127.0.0.1
-PORT=8080
-TIMEOUT=1800
+NUM_THREADS=1    # set this to the number of cores on your machine (or a bit less)
+HOST=127.0.0.1   # set this to 0.0.0.0 instead of 127.0.0.1 to allow access from other computers
+PORT=8080        # set this to a port number that is not already in use
+TIMEOUT=1800     # kill the server thread if it takes more than this many seconds to compute a response
+
+# start the gunicorn server
 gunicorn -w ${NUM_THREADS} -t ${TIMEOUT} -b ${HOST}:${PORT}  server:app
