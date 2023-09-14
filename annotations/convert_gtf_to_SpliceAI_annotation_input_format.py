b99714d4aec81a8be7060aebd3bfa4869ff1b4a1
bw2 <ben.weisburd@gmail.com>
2023-09-14 15:39:03 -0400

    discard redundant transcripts (non-canonical transcripts that have the same chrom/tx_start/tx_end/strand as other transcripts) from the SpliceAI annotation file to avoid clutter in genes with many transcripts that only differ in their internal exon structures

--- annotations/convert_gtf_to_SpliceAI_annotation_input_format.py
+++ annotations/convert_gtf_to_SpliceAI_annotation_input_format.py
@@ -2,6 +2,7 @@
 
 import argparse
 import collections
+import json
 import os
 import pandas as pd
 import re
@@ -14,11 +15,19 @@ def main():
         and outputs an annotation file which can be passed to SpliceAI instead of 
         the default SpliceAI annotations which are still on Gencode v24.""")
 
+    p.add_argument("-a", "--annotation-json-path", required=True, help="Path of the transcript annotations JSON file "
+                   "created by generate_transcript_annotation_json.py")
     p.add_argument("gtf_gz_path", help="Path of gene annotations file in GTF format")
     args = p.parse_args()
 
-    print(f"Parsing {args.gtf_gz_path}")
+    for path in args.annotation_json_path, args.gtf_gz_path:
+        if not os.path.exists(path):
+            p.error(f"File not found: {path}")
 
+    with open(args.annotation_json_path, "rt") as f:
+        transcript_annotations = json.load(f)
+
+    print(f"Parsing {args.gtf_gz_path}")
     transcript_id_to_exons = collections.defaultdict(set)
     for record in parse_gtf(os.path.expanduser(args.gtf_gz_path), "exon"):
         key = (record["transcript_id"], record["strand"], record["chrom"])
@@ -27,17 +36,34 @@ def main():
             raise ValueError(f"Duplicate exon: {exon_2_tuple} in transcript {key}")
         transcript_id_to_exons[key].add(exon_2_tuple)
 
-    # reformat the aggregated records into a list which can be turned into a pandas DataFrame
     output_records = []
+    # SpliceAI predictions (prior to 'masking') depend only on transcript chrom/start/end/strand. Often, transcripts
+    # within the same gene have the same chrom/start/end/strand and differ only in their internal exon structure.
+    # We can discard these redundant transcripts (while making sure to keep all MANE Select and canonical transcripts).
+    output_records_transcript_keys = set()
+    maybe_output_records = []
     for (transcript_id, strand, chrom), exon_set in transcript_id_to_exons.items():
         tx_start_0based = min([start_1based - 1 for start_1based, _ in exon_set])
         tx_end_1based = max([end_1based for _, end_1based in exon_set])
+        transcript_id_without_version = transcript_id.split(".")[0]
+        if transcript_id_without_version not in transcript_annotations:
+            print(f"WARNING: transcript {transcript_id_without_version} not found in {args.annotation_json_path}")
+            continue
+        transcript_annotation = transcript_annotations[transcript_id_without_version]
+        if transcript_annotation['t_priority'] == "N":
+            output_list = maybe_output_records
+        else:
+            output_list = output_records
+            transcript_key = (chrom, strand, str(tx_start_0based), str(tx_end_1based))
+            output_records_transcript_keys.add(transcript_key)
 
+        # if it's a MANE Select, MANE Plus Clinical or canonical transcript
         exon_list = sorted(list(exon_set))
         exon_starts_0based = [start_1based - 1 for start_1based, _ in exon_list]
         exon_ends_1based = [end_1based for _, end_1based in exon_list]
 
-        output_records.append({
+        # reformat the records into a list which can be turned into a pandas DataFrame
+        output_list.append({
             "#NAME": transcript_id,
             "CHROM": chrom,
             "STRAND": strand,
@@ -46,6 +72,23 @@ def main():
             "EXON_START": ",".join([str(s) for s in exon_starts_0based]) + ",",
             "EXON_END": ",".join([str(s) for s in exon_ends_1based]) + ",",
         })
+
+    transcripts_kept_counter1 = len(output_records)
+    transcripts_kept_counter2 = 0
+    for output_record in maybe_output_records:
+        transcript_key = (output_record["CHROM"], output_record["STRAND"], output_record["TX_START"], output_record["TX_END"])
+        if transcript_key not in output_records_transcript_keys:
+            # if this transcript has a chrom/start/end/strand that hasn't been seen before, add it to the output
+            output_records_transcript_keys.add(transcript_key)
+            output_records.append(output_record)
+            transcripts_kept_counter2 += 1
+
+    assert transcripts_kept_counter1 + transcripts_kept_counter2 == len(output_records)
+    print(f"Kept {transcripts_kept_counter1:,d} transcripts which were MANE Select, MANE Plus Clinical or canonical.")
+    print(f"Kept {transcripts_kept_counter2:,d} additional transcripts with unique transcript start/stop coords.")
+    print(f"Discarded {len(maybe_output_records) - transcripts_kept_counter2:,d} out of {len(transcript_id_to_exons):,d} "
+          f"({(len(maybe_output_records) - transcripts_kept_counter2) / len(transcript_id_to_exons):.1%}) transcripts "
+          f"because they were redundant.")
 
     output_df = pd.DataFrame(output_records)
     output_df = output_df[["#NAME", "CHROM", "STRAND", "TX_START", "TX_END", "EXON_START", "EXON_END"]]
--- annotations/update_SpliceAI_annotation_txt_files.sh
+++ annotations/update_SpliceAI_annotation_txt_files.sh
@@ -2,7 +2,8 @@ set -ex
 
 gencode_version=v44
 for p in gencode.${gencode_version}.basic.annotation.gtf.gz  gencode.${gencode_version}lift37.basic.annotation.gtf.gz; do
-    log_path=process_$(echo $p | sed s/.gtf.gz//).log
-    time python3 generate_transcript_annotation_json.py $p | tee -a ${log_path}    
-    time python3 convert_gtf_to_SpliceAI_annotation_input_format.py $p  | tee ${log_path}
+    log_path=process_$(echo ${p} | sed s/.gtf.gz//).log
+    time python3 generate_transcript_annotation_json.py ${p} | tee -a ${log_path}
+    json_path=$(echo ${p} | sed 's/.gtf.gz/.transcript_annotations.json/')
+    time python3 convert_gtf_to_SpliceAI_annotation_input_format.py -a ${json_path} ${p}  | tee ${log_path}
 done
