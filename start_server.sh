4a35e653a89b9223b1947a9b6aff65dc966ac1aa
bw2 <ben.weisburd@gmail.com>
2023-12-05 20:09:15 -0500

    restart gunicorn if it dies

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,12 @@
 set -x
 
 #redis-cli flushall  #  clear all keys from redis
-
+while true
+do
+      
 gunicorn -w 8 -t 1800 -b 0.0.0.0:80  -b 0.0.0.0:443 \
   --keyfile=../spliceailookup-api.broadinstitute.org.key \
   --certfile=../spliceailookup-api.broadinstitute.org.crt \
   server:app
+
+done
