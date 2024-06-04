4a3cba7a9dae5ef5b9e87f0177762e735c881e2b
bw2 <ben.weisburd@gmail.com>
2024-06-04 08:11:27 -0400

    updated db queries

--- google_cloud_run_services/connect_to_db.sh
+++ google_cloud_run_services/connect_to_db.sh
@@ -1,4 +1,12 @@
 #set -ex
 
-echo DB password: $(cat db_password.txt)
-psql -h 34.173.33.168 -d spliceai-lookup-db -U postgres -W -d spliceai-lookup-db
+PGPASSWORD=$(cat .pgpass) psql -h 34.173.33.168 -d spliceai-lookup-db -U postgres -d spliceai-lookup-db
+
+
+# useful queries:
+
+# count variant consequences (counted once per variant)
+# select variant_consequence, count(*) as c from (select variant_consequence, variant from log where length(variant_consequence) > 1 group by variant_consequence, variant) log group by variant_consequence order by c desc;
+
+# count queries per ip per day
+# select ip, logtime::timestamp::date, MAX(event_name), count(*) as c from log group by ip, logtime::timestamp::date ORDER BY logtime::timestamp::date desc, c desc
