227eea89f620a4037bf5a1512cbd7e26e5c5ae97
bw2 <ben.weisburd@gmail.com>
2024-10-14 10:46:42 -0400

    more logging

--- google_cloud_run_services/test_score_consistency.py
+++ google_cloud_run_services/test_score_consistency.py
@@ -19,20 +19,30 @@ p.add_argument("-n", type=int, help="number of rows to query", default=1000)
 p.add_argument("-p", "--show-progress-bar", action="store_true")
 args, _ = p.parse_known_args()
 
-
+days_ago = 5
 conn = psycopg2.connect(f"dbname='{args.db}' user='{args.user}' host='{args.ip}' password='{args.password}'")
-df = pd.read_sql_query(f"SELECT key, value FROM cache WHERE accessed < now() - INTERVAL '5 days' ORDER BY accessed ASC", conn)
-counter = collections.Counter()
+#query = f"SELECT key, value, accessed FROM cache WHERE accessed < now() - INTERVAL '{days_ago} days' ORDER BY accessed ASC"
+query = f"SELECT key, value, accessed FROM cache WHERE key like 'pangolin%hg38%' AND accessed < now() - INTERVAL '{days_ago} days' ORDER BY accessed ASC"
+df = pd.read_sql_query(query, conn)
+print(f"Retrieved {len(df):,d} records from cache that were last accessed more than {days_ago} days ago.")
+if args.n:
+    keep_every_kth_record = len(df)//args.n
+    df = df[df.index % keep_every_kth_record == 0]
+    print(f"Kept {len(df):,d} records after applying -n {args.n} arg")
 
-iterator = zip(df.key, df.value)
+counter = collections.Counter()
+iterator = zip(df.key, df.value, df.accessed)
 if args.show_progress_bar:
     iterator = tqdm.tqdm(iterator, total=len(df), unit=" variants", unit_scale=True)
 
-for i, (cache_key, cache_value) in enumerate(iterator):
-    print(f"{i+1:3,d}: Processing", cache_key)
+for i, (cache_key, cache_value, last_accessed) in enumerate(iterator):
+    print(f"{i+1:3,d}: Processing", cache_key, "which was last accessed on", last_accessed)
     data = json.loads(cache_value)
-    if "spliceai" not in data["source"]:
+
+    if not data.get("scores"):
+        print("ERROR: No scores found in cached value. Skipping...")
         continue
+
     tool = data["source"].split(":")[0]
     hg = data["genomeVersion"]
     distance = data["distance"]
@@ -40,56 +50,81 @@ for i, (cache_key, cache_value) in enumerate(iterator):
     mask = cache_key[-1]
     variant = data["variant"]
 
-    data["scores"] = list(sorted(data["scores"], key=lambda s: s.get("t_id")))
-    cached_scores = data["scores"]
-    counter[f"   {tool}"] += 1
-    counter[f"  hg{hg}"] += 1
-    counter[f" m{mask}"] += 1
-
     # get json response
     # time requests
     start_time = time.time()
     url = f"https://{tool}-{hg}-xwkwwwxdwq-uc.a.run.app/{tool}/?hg={hg}&distance={distance}&mask={mask}&variant={variant}&raw={variant}"
-    #print(url)
+    # print(url)
     try:
         response_json = requests.get(f"{url}&force=1").json()
     except Exception as e:
         print(f"ERROR: {e} when retrieving {url}  Skipping...")
         continue
 
+    if not response_json.get("scores"):
+        print(f"ERROR: {url} response doesn't contain scores: {response_json}. Skipping...")
+        continue
+
     elapsed_time = time.time() - start_time
     response_json["scores"] = list(sorted(response_json["scores"], key=lambda s: s.get("t_id")))
-    response_scores = response_json["scores"]
+    response_scores = response_json["scores"][0]
+
+    data["scores"] = list(sorted(data["scores"], key=lambda s: s.get("t_id")))
     cached_scores = data["scores"][0]
-    response_scores = response_scores[0]
+
     if not response_scores.get("t_id") or response_scores.get("t_id") != cached_scores.get("t_id"):
-        print("Transcript ids don't match:", response_scores.get("t_id"), "vs", cached_scores.get("t_id"), ". Skipping...")
+        print("Transcript ids don't match:", response_scores.get("t_id"), "vs", cached_scores.get("t_id"),
+              ". Skipping...")
         continue
+
     if not response_scores.get("g_id") or response_scores.get("g_id") != cached_scores.get("g_id"):
-        print("Gene ids don't match:", response_scores.get("g_id"), "vs", cached_scores.get("g_id"), ". Skipping...")
+        print("Gene ids don't match:", response_scores.get("g_id"), "vs", cached_scores.get("g_id"),
+              ". Skipping...")
         continue
+
+    counter[f"   {tool}"] += 1
+    counter[f"  hg{hg}"] += 1
+    counter[f" m{mask}"] += 1
 
     missing_keys = set()
     mismatched_values = set()
-    for k, v in cached_scores.items():
-        if k in ("t_refseq_ids", "t_id", "g_id"):
+    values_to_print = set()
+    for k, v1 in cached_scores.items():
+        if k in ("t_refseq_ids", "t_id", "g_id", "g_name"):
             # differences in gene ids are not important
             continue
 
         if k not in response_scores:
             missing_keys.add(k)
             continue
-        if v != response_scores[k]:
-            mismatched_values.add(k)
+
+        v2 = response_scores[k]
+        try:
+            diff = float(v1) - float(v2)
+        except:
+            diff = "?"
+
+        values_to_print.add((k, v1, v2, diff))
+        if v1 != v2:
+            mismatched_values.add((k, v1, v2, diff))
             continue
 
     if missing_keys:
-        print(f"ERROR: {cache_key} response is missing keys: {missing_keys}. Response: {json.dumps(response_json, indent=1)}")
+        print(f"ERROR: {cache_key} which was last accessed on {last_accessed} is missing keys: {missing_keys}. Response: {json.dumps(response_json, indent=1)}")
 
     if mismatched_values:
-        print(f"ERROR: {cache_key} response has mismatched values for keys: {mismatched_values}")
-        print(f"	Cache: {json.dumps(data, indent=1)}")
-        print(f"	Response: {json.dumps(response_json, indent=1)}")
+        print(f"ERROR: {cache_key} which was last accessed on {last_accessed} has mismatched values for keys: "
+              f"{', '.join(sorted([t[0] for t in mismatched_values]))} "
+              f"with max delta_score_diff="
+              f"{max([abs(t[3]) for t in mismatched_values if t[0].startswith('DS')] or [None])} "
+              f"and max raw_score_diff="
+              f"{max([abs(t[3]) for t in mismatched_values if t[0].startswith('S')] or [None])} ")
+
+        for k, v1, v2, diff in sorted(values_to_print):
+            print(f"    {k}:  {v1}  vs  {v2}   diff: {diff}")
+
+        #print(f"	Cache: {json.dumps(data, indent=1)}")
+        #print(f"	Response: {json.dumps(response_json, indent=1)}")
 
         df = pd.read_sql_query(f"SELECT * FROM log WHERE variant='{variant}'", conn)
         print(f"        Log:")
