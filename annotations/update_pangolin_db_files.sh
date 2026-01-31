dc73dab41a91299c4158db71c8541d36905fe5bd
Küchler, Oliver <oliver.kuechler@bih-charite.de>
2026-01-31 19:25:22 +0100

    Refactor annotation code

--- annotations/update_pangolin_db_files.sh
+++ annotations/update_pangolin_db_files.sh
@@ -4,7 +4,7 @@ gencode_version=v49
 gzcat gencode.${gencode_version}lift37.basic.annotation.gtf.gz | sed 's/chr//g' | bgzip > gencode.${gencode_version}lift37.basic.annotation.without_chr_prefix.gtf.gz
 for p in gencode.${gencode_version}.basic.annotation.gtf.gz  gencode.${gencode_version}lift37.basic.annotation.without_chr_prefix.gtf.gz; do
     set -x
-    python3 ~/code/Pangolin/scripts/create_db.py $p &
+    python3 create_pangolin_db.py $p &
     set +x
 done
 
